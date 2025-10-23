821a01da4118be0906684aa4b5bfbd0efb6bbf91
kalr <karlmiguel01001011@gmail.com>
2025-10-23 22:14:14 +0800

    Fixed Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaFacebook, FaInstagram, FaYoutube, FaEnvelope, FaPhone } from "react-icons/fa";
 import "./Footer.css";
-import YGALogo from "./images/YGAlogo.png";
+import YGALogo from "./images/YGALogo.png";
 
 function Footer() {
   const navigate = useNavigate();
